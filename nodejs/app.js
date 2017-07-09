83c3aeea867d5af8a2e2d2432df6d4330e42819a
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2017-07-09 20:26:42 +0200

    Added non-jvm example

---
+++ nodejs/app.js
@@ -0,0 +1,12 @@
+var request = require('request');
+
+request.get(
+    'http://localhost:9876/frauds',
+    function (error, response, body) {
+        if (!error && response.statusCode >= 200 && response.statusCode < 400) {
+            console.log(body)
+        } else {
+            console.log("ERROR - status [" + response.statusCode + "]")
+        }
+    }
+);
