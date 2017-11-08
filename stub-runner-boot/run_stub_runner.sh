b0e0d56a00f03ff145328028243b0a943bf8ab27
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2017-11-08 18:39:20 +0100

    Fixed the repo

--- stub-runner-boot/run_stub_runner.sh
+++ stub-runner-boot/run_stub_runner.sh
@@ -2,7 +2,9 @@
 
 mkdir -p target
 cd target
-git clone https://github.com/spring-cloud-samples/github-analytics-stub-runner-boot
-cd github-analytics-stub-runner-boot
-./mvnw clean install
-java -jar target/github-analytics-*.jar --stubrunner.workOffline="true" --stubrunner.ids="com.example:fraud-detection"
+git clone https://github.com/spring-cloud-samples/stub-runner-boot || REPO_CLONED="true"
+cd stub-runner-boot
+if [[ "${REPO_CLONED}" != "true" ]]; then
+	./mvnw clean install
+fi
+java -jar target/stub-runner-boot-*.jar --stubrunner.workOffline="true" --stubrunner.ids="com.example:fraud-detection:+:9876"
