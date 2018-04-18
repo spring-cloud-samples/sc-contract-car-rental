5c683b619ef11f6c52d741bbc3699cdcd0e51e7d
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2018-04-18 12:47:26 +0200

    Bumped samples to use Finchley

--- nodejs/run_stubrunner_as_docker.sh
+++ nodejs/run_stubrunner_as_docker.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Provide the Spring Cloud Contract Docker version
-SC_CONTRACT_DOCKER_VERSION="1.2.4.BUILD-SNAPSHOT"
+SC_CONTRACT_DOCKER_VERSION="2.0.0.BUILD-SNAPSHOT"
 # Spring Cloud Contract Stub Runner properties
-STUBRUNNER_PORT="8083"
+STUBRUNNER_PORT="8750"
 # Stub coordinates 'groupId:artifactId:version:classifier:port'
 STUBRUNNER_IDS="com.example:fraud-detection:0.0.1-SNAPSHOT:stubs:9876"
 # Run the docker with Stub Runner Boot
-docker run  --rm -e "STUBRUNNER_IDS=${STUBRUNNER_IDS}" -e "STUBRUNNER_WORK_OFFLINE=true" -p "${STUBRUNNER_PORT}:${STUBRUNNER_PORT}" -p "9876:9876"  -v "${HOME}/.m2/:/root/.m2:ro" springcloud/spring-cloud-contract-stub-runner:"${SC_CONTRACT_DOCKER_VERSION}"
+docker run  --rm -e "STUBRUNNER_IDS=${STUBRUNNER_IDS}" -e "STUBRUNNER_STUBS_MODE=LOCAL" -p "${STUBRUNNER_PORT}:${STUBRUNNER_PORT}" -p "9876:9876"  -v "${HOME}/.m2/:/root/.m2:ro" springcloud/spring-cloud-contract-stub-runner:"${SC_CONTRACT_DOCKER_VERSION}"
