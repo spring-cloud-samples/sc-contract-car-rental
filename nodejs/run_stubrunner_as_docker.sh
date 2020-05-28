96e62e6b5e338e1feeb4bae7e4ef41175b53f05c
Marcin Grzejszczak <marcin.grzejszczak@gmail.com>
2020-05-28 11:33:25 +0200

    Bumped sc-contract and boot:

--- nodejs/run_stubrunner_as_docker.sh
+++ nodejs/run_stubrunner_as_docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Provide the Spring Cloud Contract Docker version
-SC_CONTRACT_DOCKER_VERSION="2.2.0.BUILD-SNAPSHOT"
+SC_CONTRACT_DOCKER_VERSION="2.2.3.BUILD-SNAPSHOT"
 # Spring Cloud Contract Stub Runner properties
 STUBRUNNER_PORT="8750"
 # Stub coordinates 'groupId:artifactId:version:classifier:port'
