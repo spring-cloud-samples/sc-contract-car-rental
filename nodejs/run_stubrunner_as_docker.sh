2a598968d6f929c68e41b0028789a41c8ebb8fe5
Marcin Grzejszczak <marcin.grzejszczak@gmail.com>
2021-03-18 09:15:41 +0100

    Updated libs

--- nodejs/run_stubrunner_as_docker.sh
+++ nodejs/run_stubrunner_as_docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Provide the Spring Cloud Contract Docker version
-SC_CONTRACT_DOCKER_VERSION="3.0.1-SNAPSHOT"
+SC_CONTRACT_DOCKER_VERSION="3.0.3-SNAPSHOT"
 # Spring Cloud Contract Stub Runner properties
 STUBRUNNER_PORT="8750"
 # Stub coordinates 'groupId:artifactId:version:classifier:port'
