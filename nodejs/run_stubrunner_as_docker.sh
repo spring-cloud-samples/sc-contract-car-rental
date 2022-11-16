1604583eb4d79c729748b545bd2857fb67a644cd
Marcin Grzejszczak <mgrzejszczak@vmware.com>
2022-11-16 15:10:00 +0100

    Trying to fix the github actions permissions

--- nodejs/run_stubrunner_as_docker.sh
+++ nodejs/run_stubrunner_as_docker.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+export RUN_AS_SUDO="${RUN_AS_SUDO:-false}"
+export DOCKER_CMD="${DOCKER_CMD:-docker}"
+if [[ ${RUN_AS_SUDO} == "true" ]]; then
+  DOCKER_CMD="sudo ${DOCKER_CMD}"
+fi
+
 # Provide the Spring Cloud Contract Docker version
 SC_CONTRACT_DOCKER_VERSION="4.0.0-SNAPSHOT"
 # Spring Cloud Contract Stub Runner properties
@@ -6,7 +13,7 @@ STUBRUNNER_PORT="8750"
 # Stub coordinates 'groupId:artifactId:version:classifier:port'
 STUBRUNNER_IDS="com.example:fraud-detection:0.0.1-SNAPSHOT:stubs:9876"
 # Run the docker with Stub Runner Boot
-docker run  --rm -e "STUBRUNNER_IDS=${STUBRUNNER_IDS}" \
+${DOCKER_CMD} run  --rm -e "STUBRUNNER_IDS=${STUBRUNNER_IDS}" \
 -e "STUBRUNNER_STUBS_MODE=LOCAL" \
 -p "${STUBRUNNER_PORT}:${STUBRUNNER_PORT}" \
 -p "9876:9876"  \
